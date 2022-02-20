8a65cfe6ceff6d0e7fb7b02e42597f19c0a975b5
Yassir Karroum <ukarroum17@gmail.com>
2022-02-20 11:10:43 +0000

    removed token from git (oups)

--- launcher.sh
+++
@@ -1,7 +0,0 @@
-export ARBI_INFLUX_URL='http://localhost:8086'
-export ARBI_INFLUX_TOKEN='V5TV2M9vwMgquFwogTj3EbSfBRao6QrGu8vHq9WIh6OpwTtXgGZm-QaF8OSvRtlO55q8-3Nr4q4TzIG5FwOyMA=='
-export ARBI_INFLUX_ORG='binance'
-export ARBI_INFLUX_BUCKET='arbi'
-
-python datacollect.py
-
